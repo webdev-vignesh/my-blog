2284e10f9650810842798e6534569272896b9bf4
vicky <vigmurug@gmail.com>
2023-02-15 19:07:12 +0530

    unwanted space removed

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -11,7 +11,6 @@ const Article = () => {
             <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">{article.title}</h1>
             {article.content.map((paragraph, index) => 
             <p className="mx-auto leading-relaxed text-base mb-4" key={index}>{paragraph}</p>)}
-
         </div>
     )
 }
