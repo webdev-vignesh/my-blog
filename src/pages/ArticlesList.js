2e160a3d1c0e51979d975f71b171bc77e5c0829f
vicky <vigmurug@gmail.com>
2023-02-15 19:06:06 +0530

    added date in article list

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -23,8 +23,12 @@ const ArticlesList = () => {
                                     </Link>
                                     <p className="leading-relaxed mb-3">{article.content[0].substring(0, 80)}...</p>
                                     <div className="flex items-center flex-wrap">
-                                        <Link to={`/article/${article.name}`} className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                                        <Link to={`/article/${article.name}`} className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                                            Learn More
                                         </Link>
+                                        <div className="justify-end flex-1 text-right">
+                                            {article.date}
+                                    </div>
                                     </div>
                                 </div>
                             </div>
--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -3,19 +3,22 @@ const articles = [
         name: "learn-react",
         title: "The fastest way to learn React",
         thumbnail: '/images/blog1.png',
-        content: ["Let's discuss about few fastest methods to learn one of the most used JS framework by web developers around the world."]
+        content: ["Let's discuss about few fastest methods to learn one of the most used JS framework by web developers around the world."],
+        date: "2023-02-10",
     },
     {
         name: "learn-web",
         title: "The fastest way to learn Web",
         thumbnail: '/images/blog2.jpg',
-        content: ["Let's discuss about few ways to learn about web."]
+        content: ["Let's discuss about few ways to learn about web."],
+        date: "2022-11-27",
     },
     {
         name: "learn-js",
         title: "Larn JavaScript in 2 weeks",
         thumbnail: '/images/blog3.jpg',
-        content: ["Let's discuss about few methods to learn JS programming language."]
+        content: ["Let's discuss about few methods to learn JS programming language."],
+        date: "2022-09-05",
     },
 ]
 
